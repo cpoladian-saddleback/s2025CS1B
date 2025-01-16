0cd855a2fb07fae93249c370bee05139926dd420
cpoladian-saddleback <cpoladian0@saddleback.edu>
2025-01-16 04:02:44 +0000

    first commit

---
+++ p01.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int primeFactors(int num, int factors[100]) {
+    if (num < 2) {
+        return -1;
+    }
+    int count = 0;
+    int orig_num = num;
+    while (num > 1) {
+        for(int i = 2; i <= orig_num; i++) {
+            if(num % i == 0) {
+                factors[count++] = i;
+                num /= i;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    for(;;) { //infinite loop
+        cout << "Enter a number (enter -1 to exit): ";
+        int x;
+        cin >> x;
+        if (x == -1) {
+            break;
+        }
+        int factors[100];
+        int count = primeFactors(x, factors);
+        for (int i = 0; i < count; i++) {
+            cout << factors[i] << ", ";
+        }
+        cout << endl;
+    }
+}
